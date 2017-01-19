b554b094444a2760bd5025c3614b0f716216cf64
Fabricio Braga <fabraga@gmail.com>
2017-01-19 23:14:14 +0000

    Add tests for GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -59,13 +59,13 @@ describe("POST /todos", () => {
 });
 
 describe("GET /todos", () => {
-  if('should get all todos', (done) => {
+  it('should get all todos', (done) => {
     request(app)
-    .get('/todos')
-    .expect(200)
-    .expect((res) => {
-      expect(res.body.todos.length).toBe(2);
-    })
-    .end(done);
+      .get('/todos')
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todos.length).toBe(2);
+      })
+      .end(done);
   });
 });
