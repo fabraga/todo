0b9de64618536fc3312d97d28f42c456109414b1
Fabricio Braga <fabraga@gmail.com>
2017-01-25 21:30:10 +0000

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+const {ObjectID} = require('mongodb');
+
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
@@ -29,6 +31,30 @@ app.get('/todos', (req, res) => {
   });
 });
 
+// GET /todos/1234321
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  // Valid id using isValid
+    // 404 - send empty send
+  if (!ObjectID.isValid(id)) {
+     console.log('ID not valid / not found');
+     return res.status(404).send();
+  }
+
+  // findById
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      console.log('ID not valid / not found');
+      return res.status(404).send();
+    }
+    res.send({todo});
+
+  }).catch((e) => {
+    console.log('ID not found / bad request');
+    res.status(400).send();
+  });
+});
 
 app.listen(3000, () => {
   console.log('Started on port 3000');
